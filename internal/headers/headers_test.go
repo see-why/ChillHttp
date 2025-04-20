709949e9f6b2467396e941dd84fb2a383cad6615
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-04-19 23:59:11 -0400

    Add unit tests for header parsing functionality

---
+++ internal/headers/headers_test.go
@@ -0,0 +1,67 @@
+package headers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidSingleHeader(t *testing.T) {
+	headers := NewHeaders()
+	data := []byte("Host: localhost:42069\r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 23, n)
+	assert.False(t, done)
+}
+
+func TestValidSingleHeaderWithExtraWhitespace(t *testing.T) {
+	headers := NewHeaders()
+	data := []byte("Host:    localhost:42069    \r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 30, n)
+	assert.False(t, done)
+}
+
+func TestValidTwoHeadersWithExistingHeaders(t *testing.T) {
+	headers := NewHeaders()
+	headers["Existing"] = "value"
+
+	data := []byte("Host: localhost:42069\r\nContent-Type: application/json\r\n\r\n")
+
+	// Parse first header
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 23, n)
+	assert.False(t, done)
+
+	// Parse second header
+	n, done, err = headers.Parse(data[23:])
+	require.NoError(t, err)
+	assert.Equal(t, "application/json", headers["Content-Type"])
+	assert.Equal(t, 32, n)
+	assert.False(t, done)
+}
+
+func TestValidDone(t *testing.T) {
+	headers := NewHeaders()
+	data := []byte("\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	assert.Equal(t, 2, n)
+	assert.True(t, done)
+}
+
+func TestInvalidSpacingHeader(t *testing.T) {
+	headers := NewHeaders()
+	data := []byte("       Host : localhost:42069       \r\n\r\n")
+	n, done, _ := headers.Parse(data)
+	assert.Equal(t, 38, n)
+	assert.False(t, done)
+}
