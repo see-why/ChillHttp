5b876817e5fb86b09146dbba0271e68aba35d837
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-04-23 23:36:36 -0400

    Remove unused getLinesChannel function from TCP listener

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -2,50 +2,9 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"myhttpprotocol/internal/request"
 	"net"
-	"strings"
 )
-
-func getLinesChannel(reader io.ReadCloser) <-chan string {
-	ch := make(chan string)
-
-	go func() {
-		defer close(ch)
-		defer reader.Close()
-
-		buffer := make([]byte, 8)
-		currentLine := ""
-
-		for {
-			n, err := reader.Read(buffer)
-			if err != nil {
-				if err == io.EOF {
-					break
-				}
-				fmt.Printf("Error reading: %v\n", err)
-				return
-			}
-
-			parts := strings.Split(string(buffer[:n]), "\n")
-
-			for i := 0; i < len(parts)-1; i++ {
-				currentLine += parts[i]
-				ch <- currentLine
-				currentLine = ""
-			}
-
-			currentLine += parts[len(parts)-1]
-		}
-
-		if currentLine != "" {
-			ch <- currentLine
-		}
-	}()
-
-	return ch
-}
 
 func main() {
 	listener, err := net.Listen("tcp", ":42069")
