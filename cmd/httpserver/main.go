976287cc7de539a831110f68e8f100b00e5c9bb0
Iyadi Cyril Osita <iyadicy@gmail.com>
2025-04-24 21:14:42 -0400

    Add custom HTTP handler for specific request responses

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"chillhttp/internal/request"
 	"chillhttp/internal/server"
 
 	"github.com/pingcap/log"
@@ -13,8 +15,26 @@ import (
 
 const port = 42069
 
+func HttpHandler(w io.Writer, req *request.Request) (error *server.HandlerError) {
+	switch req.RequestLine.RequestTarget {
+	case "/yourproblem":
+		return &server.HandlerError{
+			Code: 400,
+			Err:  "Your problem is not my problem",
+		}
+	case "/myproblem":
+		return &server.HandlerError{
+			Code: 500,
+			Err:  "Woopsie, my bad",
+		}
+	default:
+		w.Write([]byte("All good, frfr\n"))
+		return nil
+	}
+}
+
 func main() {
-	server, err := server.Serve(port)
+	server, err := server.Serve(port, HttpHandler)
 	if err != nil {
 		fmt.Println("Error starting server: %w", err)
 	}
